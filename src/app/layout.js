05dcda1c15708246e09d87290da19948692759c7
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2025-08-02 20:41:47 -0400

    Added layout, styles, and text

---
+++ src/app/layout.js
@@ -0,0 +1,19 @@
+import "./globals.css";
+import { Lexend } from "next/font/google";
+
+const lexend = Lexend({
+    weight: ["400"],
+    subsets: ["latin"]
+})
+
+export default function RootLayout({ children }) {
+    return (
+        <>
+            <html lang="en">
+                <body className={lexend.className}>
+                    { children }
+                </body>
+            </html>
+        </>
+    );
+}
