734b5ffbce7ac9f736643b654d7361d928163cc5
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2025-08-04 11:36:15 -0400

    Added ability to select from multiple stories, added "Eating" story

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,14 +4,14 @@ import { Lexend } from "next/font/google";
 export const lexend = Lexend({
     weight: ["400"],
     subsets: ["latin"]
-})
+});
 
 export default function RootLayout({ children }) {
     return (
         <>
             <html lang="en">
                 <body className={lexend.className}>
-                    { children }
+                    {children}
                 </body>
             </html>
         </>
